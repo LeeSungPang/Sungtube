942380fe13e381fee95d427f7b82b568fac99279
이성준 <sungpang@iseongjun-ui-MacBookPro.local>
2020-02-21 23:42:01 +0900

    MongoDB&Scss&start Passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -13,7 +14,16 @@ export const postJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
   } else {
     // To Do: Register User
-    // To Do: Log user in
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
+    // To Do: Log user in``
     res.redirect(routes.home);
   }
 };
@@ -26,7 +36,6 @@ export const logout = (req, res) => {
   // To Do: Process Log Out
   res.redirect(routes.home);
 };
-
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
